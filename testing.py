b72bb80d27310b91e92bf64004763bd5b48c93fc
Jonas Zinis <jonzilysetc@gmail.com>
2025-05-21 11:16:29 +0300

    small update

--- testing.py
+++ testing.py
@@ -17,7 +17,7 @@ vectorizer = TfidfVectorizer(max_features=10000, ngram_range=(1,2))
 X_train_tfidf = vectorizer.fit_transform(X_train)
 X_test_tfidf = vectorizer.transform(X_test)
 
-model = LogisticRegression(max_iter=2000, solver="saga", C=0.01, n_jobs=-1)
+model = LogisticRegression(max_iter=2000, solver="saga", C=0.1, n_jobs=-1)
 model.fit(X_train_tfidf, y_train)
 
 y_pred = model.predict(X_test_tfidf)
